2043ae925e1f2b73bd3c6bc7c473da121cdb9f75
Olha Khomych <o.fedorchuk1@gmail.com>
2023-05-17 10:43:37 +0300

    comment migration start

--- src/main/java/dev/danit_fs4/Main.java
+++ src/main/java/dev/danit_fs4/Main.java
@@ -18,7 +18,10 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        DataBase.checkAndApplyDeltas();
+
+        // міграція
+        // DataBase.checkAndApplyDeltas();
+
         Connection connection = DataBase.connect().orElseThrow();
 
 
