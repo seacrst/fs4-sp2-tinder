4657dc13c347165f7ecf3dc2ca278cac00299816
Oleksandr Kobets <cosmopolicy@gmail.com>
2023-05-22 19:11:37 +0300

    added: working MessageServlet

--- src/main/java/dev/danit_fs4/Entity/Message.java
+++ src/main/java/dev/danit_fs4/Entity/Message.java
@@ -5,33 +5,43 @@ import dev.danit_fs4.DAO.Identifiable;
 public class Message implements Identifiable {
     private final Integer id;
     private final String createdAt;
+    private final String body;
     private final Integer idSender;
     private final Integer idReceiver;
-    private final String body;
-    private final String nameSender;
-    private final String nameReceiver;
 
-    public Message(Integer id, String createdAt, String body, Integer idSender, Integer idReceiver, String nameSender, String nameReceiver) {
+//    private final String nameSender;
+//    private final String nameReceiver;
+
+    public Message(Integer id, String createdAt, String body, Integer idSender, Integer idReceiver) {
         this.id = id;
         this.createdAt = createdAt;
         this.body = body;
         this.idSender = idSender;
         this.idReceiver = idReceiver;
-        this.nameSender = nameSender;
-        this.nameReceiver = nameReceiver;
+//        this.nameSender = nameSender;
+//        this.nameReceiver = nameReceiver;
+    }
+    public Message( String body, Integer idSender, Integer idReceiver) {
+        this.id = null;
+        this.createdAt = null;
+        this.body = body;
+        this.idSender = idSender;
+        this.idReceiver = idReceiver;
+//        this.nameSender = null;
+//        this.nameReceiver = null;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getNameSender() {
-        return nameSender;
-    }
+//    public String getNameSender() {
+//        return nameSender;
+//    }
 
-    public String getNameReceiver() {
-        return nameReceiver;
-    }
+//    public String getNameReceiver() {
+//        return nameReceiver;
+//    }
 
     public String getCreatedAt() {
         return createdAt;
--- src/main/java/dev/danit_fs4/Servlet/MessageServlet.java
+++ src/main/java/dev/danit_fs4/Servlet/MessageServlet.java
@@ -1,16 +1,101 @@
 package dev.danit_fs4.Servlet;
 
+import dev.danit_fs4.DAO.MessageDataBaseDao;
+import dev.danit_fs4.DAO.UserDatabaseDao;
+import dev.danit_fs4.Entity.Chat;
+import dev.danit_fs4.Entity.Message;
+import dev.danit_fs4.Utils.Auth;
+import dev.danit_fs4.Utils.ViewMessage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class MessageServlet extends HttpServlet {
+
+    private final MessageDataBaseDao messages;
+    private final UserDatabaseDao users;
+    private Integer hostId;
+
+    public MessageServlet(MessageDataBaseDao messages, UserDatabaseDao users) {
+        this.messages = messages;
+        this.users = users;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> answerMap = req.getParameterMap();
+        int idReceiver;
+        try {
+            idReceiver = Integer.parseInt(answerMap.get("idReceiver")[0]);
+            messages.save(new Message(
+                    answerMap.get("body")[0],
+                    Integer.parseInt(answerMap.get("idSender")[0]),
+                    idReceiver
+            ));
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+        resp.sendRedirect("/messages/" + idReceiver);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        //Отримуємо ID користувача в hostID
+        setActiveUser(req);
 
-        ResourcesOps.writeInto(resp, null, "chat.ftl");
+        //Отримуємо ID гостя в guestID
+        int guestId = Integer.parseInt(req.getPathInfo().substring(1));
+
+        //Отримуємо історію листування між користувачем та гостем
+        List<Message> messagesHistory;
+        try {
+            messagesHistory = messages.loadHistory(hostId, guestId);
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Отримуємо імена користувача та гостя в hostName та guestName
+        String hostName;
+        String guestName;
+        try{
+            hostName = users.load(hostId).get().getName();
+            guestName = users.load(guestId).get().getName();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // формуємо чат-об'єкт
+        Chat chat = new Chat(messagesHistory, hostId, guestId, hostName, guestName);
+
+        //якщо історія листування порожня - використовуємо ftl з чистим чатиком
+        PrintWriter writer = resp.getWriter();
+        ViewMessage view = new ViewMessage("/templates");
+        if (messagesHistory.size() != 0)
+            view.renderMessages(writer, chat, "chat_exist.ftl");
+        else
+            view.renderMessages(writer, chat, "chat_empty.ftl");
     }
+
+    private void setActiveUser(HttpServletRequest req){
+        try {
+            if(Auth.getCookie(req).isPresent()) {
+                Optional<Integer> id = users.getId(Auth.getCookie(req).get());
+                id.ifPresent(integer -> hostId = integer);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
