cbf45da3ffba1ed099e48f1e4de6ed1d6cf432a2
Oleksandr Kobets <cosmopolicy@gmail.com>
2023-05-23 09:52:51 +0300

    - new MessageService - MessageServlet without DAO

--- src/main/java/dev/danit_fs4/Servlet/MessageServlet.java
+++ src/main/java/dev/danit_fs4/Servlet/MessageServlet.java
@@ -1,12 +1,10 @@
 package dev.danit_fs4.Servlet;
 
-import dev.danit_fs4.DAO.MessageDataBaseDao;
 import dev.danit_fs4.Entity.Chat;
-import dev.danit_fs4.Entity.Message;
 import dev.danit_fs4.Utils.Auth;
 import dev.danit_fs4.Utils.ViewMessage;
 import dev.danit_fs4.services.AccountService;
-import dev.danit_fs4.services.UserService;
+import dev.danit_fs4.services.MessageService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,82 +12,119 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class MessageServlet extends HttpServlet {
 
-    private final MessageDataBaseDao messages = new MessageDataBaseDao();
-//    private final UserDatabaseDao users;
-    private final UserService users = new UserService();
+    private final MessageService chatService = new MessageService();
     private final AccountService accountService = new AccountService();
-    private Integer hostId;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Map<String, String[]> answerMap = req.getParameterMap();
-        int idReceiver;
-        try {
-            idReceiver = Integer.parseInt(answerMap.get("idReceiver")[0]);
-            messages.save(new Message(
-                    answerMap.get("body")[0],
-                    Integer.parseInt(answerMap.get("idSender")[0]),
-                    idReceiver
-            ));
-        } catch (
-                SQLException e) {
-            throw new RuntimeException(e);
-        }
+        int idReceiver = Integer.parseInt(answerMap.get("idReceiver")[0]);
+        chatService.saveMessage(
+                answerMap.get("body")[0],
+                Integer.parseInt(answerMap.get("idSender")[0]),
+                idReceiver);
         resp.sendRedirect("/messages/" + idReceiver);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        //Отримуємо ID користувача в hostID
-        setActiveUser(req);
-
-        //Отримуємо ID гостя в guestID
+        int hostId = getHostId(req);
         int guestId = Integer.parseInt(req.getPathInfo().substring(1));
+        Chat chat = chatService.getChat(hostId, guestId);
 
-        //Отримуємо історію листування між користувачем та гостем
-        List<Message> messagesHistory;
-        try {
-            messagesHistory = messages.loadHistory(hostId, guestId);
-        } catch (
-                SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        //Отримуємо імена користувача та гостя в hostName та guestName
-        String hostName;
-        String guestName;
-
-            hostName = users.getUser(hostId).get().getName();
-            guestName = users.getUser(guestId).get().getName();
-
-
-        // формуємо чат-об'єкт
-        Chat chat = new Chat(messagesHistory, hostId, guestId, hostName, guestName);
-
-        //якщо історія листування порожня - використовуємо ftl з чистим чатиком
         PrintWriter writer = resp.getWriter();
-        ViewMessage view = new ViewMessage("/templates");
-        if (messagesHistory.size() != 0)
-            view.renderMessages(writer, chat, "chat_exist.ftl");
+        ViewMessage vm = new ViewMessage("/templates");
+        if (chat.getHistory().size() != 0)
+            vm.renderMessages(writer, chat, "chat_exist.ftl");
         else
-            view.renderMessages(writer, chat, "chat_empty.ftl");
+            vm.renderMessages(writer, chat, "chat_empty.ftl");
     }
 
-    private void setActiveUser(HttpServletRequest req){
-
-            if(Auth.getCookie(req).isPresent()) {
-                Integer id = accountService.getId(Auth.getCookie(req).get());
-                hostId = id;
-            }
-
+    private int getHostId(HttpServletRequest req) {
+        Integer id = null;
+        if (Auth.getCookie(req).isPresent()) {
+            id = accountService.getId(Auth.getCookie(req).get());
+        }
+        return id;
     }
-
 }
+
+
+//
+//
+//    private final MessageDataBaseDao messages = new MessageDataBaseDao();
+////    private final UserDatabaseDao users;
+//    private final UserService users = new UserService();
+//    private final AccountService accountService = new AccountService();
+//    private Integer hostId;
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        Map<String, String[]> answerMap = req.getParameterMap();
+//        int idReceiver;
+//        try {
+//            idReceiver = Integer.parseInt(answerMap.get("idReceiver")[0]);
+//            messages.save(new Message(
+//                    answerMap.get("body")[0],
+//                    Integer.parseInt(answerMap.get("idSender")[0]),
+//                    idReceiver
+//            ));
+//        } catch (
+//                SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        resp.sendRedirect("/messages/" + idReceiver);
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//        //Отримуємо ID користувача в hostID
+//        setActiveUser(req);
+//
+//        //Отримуємо ID гостя в guestID
+//        int guestId = Integer.parseInt(req.getPathInfo().substring(1));
+//
+//        //Отримуємо історію листування між користувачем та гостем
+//        List<Message> messagesHistory;
+//        try {
+//            messagesHistory = messages.loadHistory(hostId, guestId);
+//        } catch (
+//                SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        //Отримуємо імена користувача та гостя в hostName та guestName
+//        String hostName;
+//        String guestName;
+//
+//            hostName = users.getUser(hostId).get().getName();
+//            guestName = users.getUser(guestId).get().getName();
+//
+//
+//        // формуємо чат-об'єкт
+//        Chat chat = new Chat(messagesHistory, hostId, guestId, hostName, guestName);
+//
+//        //якщо історія листування порожня - використовуємо ftl з чистим чатиком
+//        PrintWriter writer = resp.getWriter();
+//        ViewMessage vm = new ViewMessage("/templates");
+//        if (messagesHistory.size() != 0)
+//            vm.renderMessages(writer, chat, "chat_exist.ftl");
+//        else
+//            vm.renderMessages(writer, chat, "chat_empty.ftl");
+//    }
+//
+//    private void setActiveUser(HttpServletRequest req){
+//
+//            if(Auth.getCookie(req).isPresent()) {
+//                Integer id = accountService.getId(Auth.getCookie(req).get());
+//                hostId = id;
+//            }
+//
+//    }
+//
+//}
--- src/main/java/dev/danit_fs4/services/MessageService.java
+++ src/main/java/dev/danit_fs4/services/MessageService.java
@@ -1,7 +1,73 @@
 package dev.danit_fs4.services;
 
 import dev.danit_fs4.DAO.MessageDataBaseDao;
+import dev.danit_fs4.DAO.UserDao;
+import dev.danit_fs4.Entity.Chat;
+import dev.danit_fs4.Entity.Message;
+import dev.danit_fs4.Entity.User;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class MessageService {
     private final MessageDataBaseDao messages = new MessageDataBaseDao();
+    private final UserDao users = new UserDao();
+
+    //Зберігає месседж в БД
+    public void saveMessage(String body, int hostId, int guestId) {
+        try {
+            messages.save(new Message(
+                    body,
+                    hostId,
+                    guestId
+            ));
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Повертає історію листування між користувачем та гостем
+    public List<Message> getMessagesHistory(int hostId, int guestId) {
+        List<Message> messagesHistory;
+        try {
+            messagesHistory = messages.loadHistory(hostId, guestId);
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return messagesHistory;
+    }
+
+    //Повертає ім'я користувача
+    public String getHostName(int hostId) {
+        String result = "Users name not found";
+        try {
+            Optional<User> user = users.load(hostId);
+            if (user.isPresent()) result = user.get().getName();
+        } catch (SQLException s) {
+            throw new RuntimeException(s);
+        }
+        return result;
+    }
+
+    //Повертає ім'я гостя
+    public String getGuestName(int guestId) {
+        String result = "Guests name not found";
+        try {
+            Optional<User> guest = users.load(guestId);
+            if (guest.isPresent()) result = guest.get().getName();
+        } catch (SQLException s) {
+            throw new RuntimeException(s);
+        }
+        return result;
+    }
+
+    // Повертає чат-об'єкт
+    public Chat getChat(int hostId, int guestId) {
+        return new Chat(getMessagesHistory(hostId, guestId), hostId, guestId, getHostName(hostId), getGuestName(guestId));
+    }
 }
+
+
